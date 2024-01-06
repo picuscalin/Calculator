30d7e236a1cfd069e9e75c7e4588c6f9d85302d6
picuscalin <picuscalin@yahoo.com>
2024-01-06 11:06:12 +0200

    created operate function

--- calc.js
+++ calc.js
@@ -14,4 +14,22 @@ const divide =function(a,b){
     return a / b ;
 }
 
-console.log(subtract(10,2))
+let firstNum;
+let secondNum;
+let operator;
+
+function operate(a,b,operator){
+    if (operator == "+"){
+        add(a,b)
+    }
+    if (operator == "-"){
+        subtract(a,b)
+    }
+    if (operator == "*"){
+        multiply(a,b)
+    }
+    if (operator == "/"){
+        divide(a,b)
+    }
+}
+
